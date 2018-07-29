dc5fe9886382f87d0e69a2b902033a35c1bd3b44
Rafael Camargo <contato@rafaelcamargo.com>
2018-07-29 17:47:13 -0300

    Syntax highlight early stage

---
+++ src/scripts/services/code/code.js
@@ -0,0 +1,7 @@
+const _public = {};
+
+_public.highlight = plainCode => {
+
+}
+
+export default _public;
---
+++ src/scripts/services/code/code.test.js
@@ -0,0 +1,21 @@
+import codeService from './code';
+
+describe('Code Service', () => {
+
+  it('should highlight reserved words', () => {
+
+  });
+
+  it('should highlight string notation', () => {
+
+  });
+
+  it('should highlight object notation', () => {
+
+  });
+
+  it('should highlight function notation', () => {
+
+  });
+
+});
